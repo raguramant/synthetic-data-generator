0c5c05d1b44dc936703489ab7083ba3fbc09dca6
Raguraman Thiruvengadam <rahuraman87@gmail.com>
2025-06-29 19:28:42 +0530

    Checkin

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
 from peft import get_peft_model
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+import torch
+
+
+model_name = "mistralai/Mistral-7B-Instruct-v0.3"
+quantization_config  = BitsAndBytesConfig(
+    load_in_8bit=True,
+    llm_int8_enable_fp32_cpu_offload=True
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    quantization_config=quantization_config ,
+    device_map="auto",
+    trust_remote_code=True
+)
